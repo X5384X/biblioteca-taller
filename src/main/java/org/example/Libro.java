4def81b8f15a6ffad3543f51e97db9e38efaeb18
Seba Yáñez <111529755+X5384X@users.noreply.github.com>
2024-05-05 17:47:49 -0400

    Revert "Develop v0.3"

--- src/main/java/org/example/Libro.java
+++ src/main/java/org/example/Libro.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +10,6 @@ public class Libro {
     private int ejemplaresDisponibles;
     private List<Integer> calificaciones;
     private List<String> comentarios;
-    private LocalDate fechaPrestamo;
-    private LocalDate fechaDevolucion;
 
     public Libro(String titulo, String autor, String categoria, int ejemplaresDisponibles) {
         this.titulo = titulo;
@@ -22,58 +19,13 @@ public class Libro {
         this.calificaciones = new ArrayList<>();
         this.comentarios = new ArrayList<>();
     }
-    
-    public String getTitulo() {
-        return titulo;
+
+    public void agregarCalificacion(int calificacion) {
+        calificaciones.add(calificacion);
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    
-    public String getAutor() {
-        return autor;
+    public void agregarComentario(String comentario) {
+        comentarios.add(comentario);
     }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public int getEjemplaresDisponibles() {
-        return ejemplaresDisponibles;
-    }
-
-    public void setEjemplaresDisponibles(int ejemplaresDisponibles) {
-        this.ejemplaresDisponibles = ejemplaresDisponibles;
-    }
-    
-    public LocalDate getFechaPrestamo() {
-        return fechaPrestamo;
-    }
-
-    public void setFechaPrestamo(LocalDate fechaPrestamo) {
-        this.fechaPrestamo = fechaPrestamo;
-    }
-
-    public LocalDate getFechaDevolucion() {
-        return fechaDevolucion;
-    }
-
-    public void setFechaDevolucion(LocalDate fechaDevolucion) {
-        this.fechaDevolucion = fechaDevolucion;
-    }
-
-    @Override
-    public String toString() {
-        return "[titulo=" + titulo + ", autor=" + autor + ", categoria=" + categoria + ", ejemplaresDisponibles="
-                + ejemplaresDisponibles + ", calificaciones=" + calificaciones + ", comentarios=" + comentarios + "]";
-    }
 }
